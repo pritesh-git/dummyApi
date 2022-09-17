e7c0cea45804af5e84ec2f2b490505af6325d7b3
pritesh-git <pritesh.bhati59@gmail.com>
2022-09-18 02:26:17 +0530

    Updated format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import PsUsers from './components/PsUsers'
+import PsTodos from './components/PsTodos'
+import PsPosts from './components/PsPosts'
+import PsCmnt from './components/PsCmnt'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Main from './components/Main'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Main />
+          <Routes>
+            <Route exact path="/" element={<PsUsers />} />
+            <Route path="/Comments" element={<PsCmnt />} />
+            <Route path="/Posts" element={<PsPosts />} />
+            <Route path="/Todos" element={<PsTodos />} />
+          </Routes>
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/PsTodos.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+class PsTodos extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todo: [],
+    }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ todo: data })
+      })
+      .catch(console.log)
+  }
+  render() {
+    return (
+      <div>
+        <center>
+          <h1>ToDo List</h1>
+        </center>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="cols">User-Id</th>
+              <th scope="cols">Id</th>
+              <th scope="cols">Title</th>
+              <th scope="cols">Completed</th>
+            </tr>
+          </thead>
+          {this.state.todo.map(todo => (
+            <tr>
+              <td>{todo.userId}</td>
+              <td>{todo.id}</td>
+              <td>{todo.title}</td>
+              <td>{todo.completed}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    )
+  }
+}
+
+export default PsTodos
